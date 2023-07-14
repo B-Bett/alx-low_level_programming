9bae96d522e745aec009bb914407db250be4ac00
Benard Bett <kipkoechbettbenard023@gmail.com>
2023-07-14 07:05:25 -0700

    0-positive_or_negative.c

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <time.h>
+/**
+ * more header goes there
+ * Return: 0(Success)
+ */
+int main(void)
+{
+int n;
+strand(time(0));
+n=rand() - RAND_MAX / 2;
+return (0);
+}
