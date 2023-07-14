6938ea7c338f114bb02786da1533e1b94455e51a
Benard Bett <kipkoechbettbenard023@gmail.com>
2023-07-14 05:06:12 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ *  main - a program that outputs or pris a line with puts function
+ *  Return: 0 (shows success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
