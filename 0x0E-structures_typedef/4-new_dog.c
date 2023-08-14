168f0131b07845c6b0cb3ec77bb0e1d9d5fd78c3
Benard Bett <kipkoechbettbenard023@gmail.com>
2023-08-14 09:49:24 -0700

    4-new_dog.c

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -4,21 +4,18 @@
 /**
  * _strlen - returns the length of a string
  * @s: string to evaluate
- *
  * Return: the length of the string
  */
 int _strlen(char *s)
 {
-	int i;
+	int j;
 
-	i = 0;
-
-	while (s[i] != '\0')
+	j = 0;
+	while (s[j] != '\0')
 	{
-		i++;
+		j++;
 	}
-
-	return (i);
+	return (j);
 }
 
 /**
@@ -27,29 +24,24 @@ int _strlen(char *s)
  * to the buffer pointed to by dest
  * @dest: pointer to the buffer in which we copy the string
  * @src: string to be copied
- *
  * Return: the pointer to dest
  */
 char *_strcpy(char *dest, char *src)
 {
-	int len, i;
+	int len, j;
 
 	len = 0;
-
 	while (src[len] != '\0')
 	{
 		len++;
 	}
-
-	for (i = 0; i < len; i++)
+	for (j = 0; j < len; j++)
 	{
-		dest[i] = src[i];
+		dest[j] = src[j];
 	}
-	dest[i] = '\0';
-
+	dest[j] = '\0';
 	return (dest);
 }
-
 /**
  * new_dog - creates a new dog
  * @name: name of the dog
